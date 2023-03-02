1f0598a34463907a2e7e403b760d3f9851edb220
Kimpulla <haukka.kimi@gmail.com>
2023-03-02 08:10:35 +0200

    Lisätty kommentointia, skannaaminen toimii.

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,16 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useEffect } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import React from 'react';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
+/**
+ * Funktio Camera skannaa viivakoodit ja QR-koodit.
+ * 
+ * Käytössä Expo Barcode Scanner kirjasto.
+ * Lisätietoja: https://docs.expo.dev/versions/v48.0.0/sdk/bar-code-scanner/
+ * 
+ * Tarkempi dokumentaatio: https://github.com/expo/expo/tree/sdk-48/packages/expo-barcode-scanner
+ * 
+ * Lisenssi ( MIT ):  https://github.com/expo/expo/blob/main/LICENSE
+ * 
+ * @returns viivakoodin
+ */
 export default function Camera() {
 
   const [skannattuData, setSkannattuData] = React.useState();
 
+  /**
+   * Tulostetaan skannattu data. 
+   * BarCodeScanner tukee koodiformaatteja Androidilla:
+   * [aztec, codabar, code39, code128, datamatrix, ean13, ean8, itf14, maxicode,
+   * pdf417, rss14, rssexpanded, upc_a, upc_e, upc_ean, qr]
+   * 
+   * Lisätietoja: https://docs.expo.dev/versions/v48.0.0/sdk/bar-code-scanner/
+   * 
+   * @param {type} viivakoodin_tyyppi
+   * @param {data} viivakoodin_data
+   */
   const viivakoodiLuettu = ({type, data}) => {
     setSkannattuData(data);
-    console.log(`Data: ${data}`);
-    console.log(`Type: ${type}`);
+    console.log('Data: ' + data);
+    console.log('Type: '+ type);
   };
 
   return (
@@ -25,6 +48,9 @@ export default function Camera() {
   );
 }
 
+/**
+ * Tyylit
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -33,3 +59,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
