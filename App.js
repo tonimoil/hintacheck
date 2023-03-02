dea3265c0d045cacb648dcdbaa8f8f2b5bfb5e43
Kimpulla <haukka.kimi@gmail.com>
2023-03-02 08:12:15 +0200

    Lisätty kommentointia. Soveellus avautuu kameraan ja skannaus toimii.

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Camera from './src/components/Camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+
 /*
+TODO: Navigaatio --> Tulokset --> jne..
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Camera } from './src/components/Camera.js'
@@ -10,7 +13,6 @@ import { History } from './src/components/History.js'
 
 const Stack = createNativeStackNavigator();
 
-  
 return (
   <NavigationContainer>
     <Stack.Navigator>
@@ -19,24 +21,23 @@ return (
     </Stack.Navigator>
   </NavigationContainer>
 );
-  
 */
 
 
 /**
- * Sovellus käynnistyy kameraan.
+ * Sovellus käynnistää kameran, jolla voidaan skannata viivakoodeja ja QR-koodeja.
+ * Funktio kysyy ensin kameran käyttöoikeutta käyttäjältä, jonka jälkeen käyttää komponenttia
+ * Camera ja siirtyy kameraan.
  * 
  * @returns componentin Camera
  */
 export default function App() {
 
-  
   const [lupaMyonnetty, setLupaMyonnetty] = React.useState(false);
-  // Kysytään käyttöoikeuksia.
   React.useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setLupaMyonnetty(status === 'myonnetty');
+      setLupaMyonnetty(status === 'granted');
     })();
   }, []);
 
