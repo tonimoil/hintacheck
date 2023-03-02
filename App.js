765aca710606777949921ab35603c835c2a7b65b
Kimpulla <haukka.kimi@gmail.com>
2023-03-02 07:33:44 +0200

    Hienosäätöä ja koodin siistimistä.

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import * as React from 'react';
-
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Camera from './src/components/Camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-
+/*
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Camera } from './src/components/Camera.js'
-import { History } from './src/components/History.js'
+import { History } from './src/components/History.js' 
 
 const Stack = createNativeStackNavigator();
 
+  
+return (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Camera" component={Camera} />
+      <Stack.Screen name="History" component={History} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+  
+*/
+
+
+/**
+ * Sovellus käynnistyy kameraan.
+ * 
+ * @returns componentin Camera
+ */
 export default function App() {
 
+  
   const [lupaMyonnetty, setLupaMyonnetty] = React.useState(false);
-
+  // Kysytään käyttöoikeuksia.
   React.useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
@@ -23,27 +42,20 @@ export default function App() {
 
   if (!lupaMyonnetty) {
     return (
-      <View style = {styles.container}>
-        <Text>Voidaksesi käyttää sovellusta on sinun annettava sovellukselle lupa käyttää kameraa.</Text>
+      <View style={styles.container}>
+        <Text>Sovellus tarvitsee oikeuden käyttää kameraa toimiakseen oikein.</Text>
       </View>
     );
   }
 
-  /*
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Camera" component={Camera} />
-        <Stack.Screen name="History" component={History} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-  */
+  return <Camera />;
 }
+/**
+ * Tyylit
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
