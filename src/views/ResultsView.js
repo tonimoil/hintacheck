5a549b721ab809567ade1923d8d936f47a3ba7cb
Kimpulla <haukka.kimi@gmail.com>
2023-04-05 22:11:04 +0300

    Muokattu, mutta erittäin kesken. Data ei kulje kameralta tälle näkymälle.

--- src/views/ResultsView.js
+++ src/views/ResultsView.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import { TopNavigationBar, SearchResult } from '../components/index';
 
-export default function ResultsView ({ route, navigation }) {
+export default function ResultsView ({ route/*, navigation*/}) {
   const { results } = route.params;
-
+/*
   const buttons = [
     { label: 'Kamera', onPress: () => navigation.navigate("Camera") }
   ];
-
+*/
   return (
     <View style={styles.container}>
-      <TopNavigationBar buttons={buttons}/>
+      
       <ScrollView>
         {results.map((result, index) => (
           <View key={index} style={styles.resultContainer}>
