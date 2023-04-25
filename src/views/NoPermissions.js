a531efa3293bd7636b13e0d80e7095ebf4f5f972
Kimpulla <haukka.kimi@gmail.com>
2023-04-25 05:16:53 +0300

    Aloitusruutu

--- src/views/NoPermissions.js
+++ src/views/NoPermissions.js
@@ -4,10 +4,15 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function NoPermissions() {
 return (
       <View style={styles.container}>
-        <Text>
-            Sovellus tarvitsee oikeuden käyttää kameraa toimiakseen oikein.
+        <Text style={styles.text}>
+            hintacheck
+        </Text>
+
+        <Text style={styles.notice}>
+          Camera access is required to use this app.
         </Text>
       </View>
+
     );
 }
 
@@ -16,5 +21,20 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
+      backgroundColor:'#3FC3D2'
     },
+    text: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    notice: {
+      flex: 1,
+      fontSize: 10,
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      position: 'absolute',
+      bottom: 0,
+      color: 'white',
+    }
   });
